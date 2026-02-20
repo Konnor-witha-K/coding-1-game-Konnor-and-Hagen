b47a822e32b313c22a17050d13ef089dec137dd8
Konnor-witha-K <s1049471@student.knoxschools.org>
2026-02-20 15:45:23 +0000

    added player to dictionary

--- student_work/game.py
+++ student_work/game.py
@@ -2,10 +2,13 @@
 
 game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+    "player": "\U0001F9D9", #🧙‍♂️ wizard
+    "room_1_width": "8", 
+    "room_1_height": "7" 
 }
 
 def draw_board(screen):
     # Print the board and all game elements using curses
 
-
+print(game_data{player})
 # Good Luck!
